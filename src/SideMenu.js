8a2067fa14b6f35e8d42dbda5b6c7e0e72103d4d
kimyazdani <yazdanikimia@yahoo.com>
2020-12-16 20:22:21 +0330

    refresh side m

--- src/SideMenu.js
+++ src/SideMenu.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import axios from "axios"; 
 
 export default class SideMenu extends Component {
+    state = {
+        logged_in: "f"
+    }
 	constructor(props) {
 		super(props);
 	}
@@ -9,6 +12,14 @@ export default class SideMenu extends Component {
     handle_logout() {
         axios.post("http://localhost:8000/api/v1/account/logout/", {}, {headers: {'Authorization': 'Bearer ' + localStorage.getItem('refresh_token')}})
         localStorage.removeItem('token')
+    }
+
+    handle_logged_in() {
+        axios.post("http://localhost:8000/api/token/refresh", { 
+              refresh: localStorage.getItem('refresh_token')
+          }).then((res) => {
+        localStorage.setItem('token', res.access);
+          this.setState({logged_in: "t"})});
     }
 
 	render() {
@@ -22,10 +33,10 @@ export default class SideMenu extends Component {
             	<div class="right menu">
            		<a href="/" class={"Home"===this.props.classIn ? "active item" : "item"}> صفحه‌ اصلی </a>
                 <a href="/ad/" class={"allads"===this.props.classIn ? "active item" : "item"}> آگهی‌ها</a>
-                {this.props.logged_in === "t"? <a href="/ad/new" class={"newad"===this.props.classIn ? "active item" : "item"}> ثبت آگهی </a>:''}
-                {this.props.logged_in === "t"? <a onClick={this.handle_logout.bind(this)} class="item"> خروج از اکانت </a>:''}
-                {this.props.logged_in === "f"? <a href="/acc/" class={"login"===this.props.classIn?"active item":"item"}> ورود به اکانت</a>:''}
-                {this.props.logged_in === "f"? <a href="/acc/register" class={"regacc"===this.props.classIn?"active item":"item"}> ساخت اکانت</a>:''}
+                {this.state.logged_in === "t"? <a href="/ad/new" class={"newad"===this.props.classIn ? "active item" : "item"}> ثبت آگهی </a>:''}
+                {this.state.logged_in === "t"? <a onClick={this.handle_logout.bind(this)} class="item"> خروج از اکانت </a>:''}
+                {this.state.logged_in === "f"? <a href="/acc/" class={"login"===this.props.classIn?"active item":"item"}> ورود به اکانت</a>:''}
+                {this.state.logged_in === "f"? <a href="/acc/register" class={"regacc"===this.props.classIn?"active item":"item"}> ساخت اکانت</a>:''}
             </div>
         </div>
     </div>
