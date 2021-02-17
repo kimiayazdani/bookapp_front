e157b3b0052f2e46c7d20e45ed527e05f52d5cd0
kimiayazdani <yazdanikimia@yahoo.com>
2021-02-17 21:40:10 +0330

    sds

--- src/Pages/Profile_else.js
+++ src/Pages/Profile_else.js
@@ -21,6 +21,9 @@ import {
 
 
 export default class ProfileElse extends Component {
+
+
+    
     state = {
         email:"yazdanikimia@gmail.com",
         image:"",
@@ -55,6 +58,62 @@ export default class ProfileElse extends Component {
         prev_rating: 0
 
     };
+
+    constructor(props) {
+    super(props);
+
+    
+                axios.post("http://127.0.0.1:8000/api/token/refresh/", { 
+              refresh: localStorage.getItem('refresh_token')
+          }).then((res) => {
+            localStorage.setItem('token', res.data.access);
+            this.setState({logged_in:true});
+          }).catch((err) => {
+
+
+          });
+        axios.get("http://127.0.0.1:8000/api/v1/account/show/"+this.state.username+"/", { headers: {'Authorization': 'Bearer  ' + localStorage.getItem('token')}}).then((res)=>{
+            this.setState({username:res.data.username, email:res.data.email, image:(res.data.avatar? res.data.avatar: ""), namename: res.data.name,
+              bio: res.data.bio})
+          }).catch((err) => {})
+        console.log(this.state.image)
+        axios
+            .get("http://localhost:8000/api/v1/book-advertise/"+this.state.username+"/posts/", { headers: {'Authorization': 'Bearer  ' + localStorage.getItem('token')}})
+            .then((res) => {
+                var a = this.state.lists
+                for (var i = 0; i < res.data.length; i++) {
+                    a.push({id: res.data[i].id, title: res.data[i].title, author: res.data[i].authorName,
+                        image:res.data[i].poster? res.data[i].poster: '',
+                        description: res.data[i].description, sell: res.data[i].ad_type, price: (res.data[i].price? res.data[i].price: 0)
+                    })
+                }
+                console.log(a)
+                this.setState({
+                    lists: a
+                })
+            })
+            .catch((err) => {
+                
+            });
+
+         axios.get("http://127.0.0.1:8000/api/v1/account/rate/"+this.state.username+"/get/", { headers: {'Authorization': 'Bearer  ' + localStorage.getItem('token')}}).then((res)=>{
+            this.setState({average_rating:res.data.rating})
+            this.setState({number_rating:res.data.number_rating})
+            this.setState({prev_rating: res.data.prev_rating})
+          }).catch((err) => {})
+         
+
+
+        
+            axios.get("http://127.0.0.1:8000/api/v1/account/rate/"+this.state.username+"/prev/", { headers: {'Authorization': 'Bearer  ' + localStorage.getItem('token')}}).then((res)=>{
+            this.setState({prev_rating: res.data.prev_rating})
+
+            if (res.data.prev_rating == null) {
+                this.setState({prev_rating: 0})
+            }
+          }).catch((err) => {})
+    
+  }
 
     redirectHandler = (val) => {
     	this.setState({
